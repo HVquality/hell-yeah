8cda3a4878c74717248d4141d714aa9a8744d0c1
HVquality <havyhilmi@gmail.com>
2025-05-14 22:50:12 +0700

    Update 2_Farmakokinetika_Pemberian_Oral.py

--- Pharmo/pages/2_Farmakokinetika_Pemberian_Oral.py
+++ Pharmo/pages/2_Farmakokinetika_Pemberian_Oral.py
@@ -44,7 +44,7 @@ else:
 
 # Berat badan → Vd
 weight_kg = st.sidebar.number_input("Berat badan (kg)", 30.0, 150.0, 70.0, step=1.0)
-Vd = weight_kg * 0.8  # L
+Vd = weight_kg * 0.7  # L
 
 # Rentang waktu (t_end)
 t_end_input = st.sidebar.text_input("Rentang waktu (jam)", value="48.0")
@@ -116,6 +116,12 @@ if is_repeated:
     col1.metric("C_ss,maks (Puncak)", f"{C_ss_max:.2f}")
     col2.metric("C_ss,min (Palung)", f"{C_ss_min:.2f}")
 
+mec = st.sidebar.number_input("MEC",
+                            min_value=0.0, max_value=1000.0, value=0.0, step=0.1)
+
+mtc = st.sidebar.number_input("MTC",
+                            min_value=0.0, max_value=1000.0, value=0.0, step=0.1)
+
 # Plot
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=t, y=C, mode='lines', name='Konsentrasi Obat'))
@@ -138,6 +144,16 @@ fig.update_layout(
     yaxis=dict(range=[0, y_max]),
     hovermode='x unified'
 )
+
+if mec > 0:
+    fig.add_hline(y=mec, line_dash="dot", line_color="green",
+                annotation_text=f"MEC: {mec} mg/L",
+                annotation_position="bottom right")
+
+if mtc > 0:
+    fig.add_hline(y=mtc, line_dash="dot", line_color="red",
+                annotation_text=f"MTC: {mtc} mg/L",
+                annotation_position="top right")
 
 # Tampilkan plot
 st.plotly_chart(fig, use_container_width=True)
